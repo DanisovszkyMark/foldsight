5645a48e9fb6e692b34c5af7b9a13bc3c4f38b2f
Danisovszky Márk <dmarkprog@gmail.com>
2024-08-07 08:08:15 +0200

    Alap SpringBoot projekt generálása

---
+++ src/main/java/hu/evocelot/foldsight/FoldsightApplication.java
@@ -0,0 +1,13 @@
+package hu.evocelot.foldsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/evocelot/foldsight/FoldsightApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.evocelot.foldsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoldsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
